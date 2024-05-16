bb35aee2fa30b1e89d5eef51681ba1ac07c6fd09
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-05-15 21:02:37 -0500

    ok

---
+++ data/mocks.py
@@ -0,0 +1,48 @@
+# ****** CALIDAD DEL AIRE ******
+#Nombre
+#Comuna
+#ICA
+#Fecha
+#Correo
+
+
+
+# **** RUIDO AMBIENTAL ****
+#Comuna
+#Dirrecion
+#Nombre
+#Decibeliodiurnos
+#Decibelionocturnos
+
+
+
+
+# **** SIEMBRE DE ARBOLES ****
+#Corregimientos
+#Hectareassembradas
+#Especiessembradas
+#Nombre
+#FEcha
+#Correo
+
+
+
+
+# **** GESTION DE RESIDUOS ****
+#comuna
+#Toneladas Recogidas
+#Tipos de Residuos
+#Correo de Contacto
+#Nombre
+#Fecha
+
+
+
+
+# **** CALIDAD DEL AGUA ****
+#comuna
+#pH
+#Nivel de Oxígeno Disuelto (mg/L)
+#Concentración de Fosfatos (mg/L)
+#Responsable del Monitoreo
+#Fecha de Muestreo
